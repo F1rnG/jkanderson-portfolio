728f7e42591a2f0b782c934d2c8fa0edeadbe90f
James Anderson <jkanderson09@gmail.com>
2026-02-07 11:59:41 -0700

    add pre-rendering for SEO and AI crawlers

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { hydrateRoot, createRoot } from 'react-dom/client'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const container = document.getElementById('root')
+
+if (container.hasChildNodes()) {
+  hydrateRoot(container, <React.StrictMode><App /></React.StrictMode>)
+} else {
+  createRoot(container).render(<React.StrictMode><App /></React.StrictMode>)
+}
